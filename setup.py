496a3092220399814445f626d001cd948eb8b6df
Ubuntu <ubuntu@ip-172-31-47-176.us-east-2.compute.internal>
2019-10-27 23:48:22 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='lyrics-generation-bot-gpt2',
+    version='1.0.0',
+    description='Lyrics Generation Bot',
+    author='Albert Jimenez Sanfiz',
+    author_email='jsalbert.me@gmail.com',
+    url='https://www.albertjimenez.xyz/',
+    packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
+    install_requires=[
+        'regex',
+        'tweepy',
+        'numpy',
+        'Pillow',
+        'jupyter'
+        ]
+)
